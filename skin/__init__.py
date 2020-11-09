554cccfdc03401768a03967538f64bcdd777f949
priyanshu2103 <priyanshu.singh2k@gmail.com>
2020-11-09 11:46:17 +0530

    face detection for ethnicity added

--- skin/__init__.py
+++ skin/__init__.py
@@ -6,13 +6,17 @@ import argparse
 if __name__ == "__main__":
 	parser=argparse.ArgumentParser()
 	parser.add_argument("--img", help="Image to be changed")
-	parser.add_argument("--col", help="Required skin color or the OTHER image")
+	parser.add_argument("--race", help="Required skin color race")
 	parser.add_argument("--res", help="Path where image is to be saved")
 	args=parser.parse_args()
 
 	with open(args.img,'rb') as inputImage:
-	    a=args.col.strip('[] ')
-	    b=a.split(',')
-	    result=change_skin(inputImage,[int(b[0].strip()),int(b[1].strip()),int(b[2].strip())],args.res)
+		if args.race=="Asian":
+			b = [180, 103, 71]
+		elif args.race=="European":
+			b = [220, 209, 194]
+		else:
+			b = [111, 79, 29]
+		result=change_skin(inputImage,b,args.res)
 	with open(args.res,'wb') as resultFile:
 		resultFile.write(result)
